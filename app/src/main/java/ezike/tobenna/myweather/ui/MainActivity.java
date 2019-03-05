38bebc56b1be9be59a4a8f49641c7b333550e611
Tobenna Ezike <Tobeydaniels@gmail.com>
2019-03-05 09:05:03 +0100

    initial commit

---
+++ app/src/main/java/ezike/tobenna/myweather/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package ezike.tobenna.myweather.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ezike.tobenna.myweather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
