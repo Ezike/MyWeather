e72d49b090c21833ddd837ecb8880b518f2098c6
Tobenna Ezike <Tobeydaniels@gmail.com>
2019-03-31 00:21:21 +0100

    closes#5 and closes#11
    add weather icons and landscape layout

--- app/src/main/java/ezike/tobenna/myweather/ui/BindingAdapters.java
+++ app/src/main/java/ezike/tobenna/myweather/ui/BindingAdapters.java
@@ -9,6 +9,7 @@ import com.github.pwittchen.weathericonview.WeatherIconView;
 import androidx.databinding.BindingAdapter;
 import ezike.tobenna.myweather.R;
 import ezike.tobenna.myweather.utils.GlideApp;
+import ezike.tobenna.myweather.utils.WeatherIconUtils;
 
 /**
  * @author tobennaezike
@@ -26,26 +27,7 @@ public class BindingAdapters {
     @BindingAdapter({"showIcon"})
     public static void showIcon(WeatherIconView iconView, String condition) {
         Context context = iconView.getContext();
-        String[] icons = new String[]{"rain", "snow", "sun", "cloud"};
-
-        if (condition != null) {
-            for (String text : icons) {
-                switch (text) {
-                    case "rain":
-                        iconView.setIconResource(context.getString(R.string.wi_rain));
-                        break;
-                    case "snow":
-                        iconView.setIconResource(context.getString(R.string.wi_snow));
-                        break;
-                    case "sun":
-                        iconView.setIconResource(context.getString(R.string.wi_day_sunny));
-                        break;
-                    case "cloud":
-                        iconView.setIconResource(context.getString(R.string.wi_cloud));
-                        break;
-                }
-            }
-        }
+        WeatherIconUtils.getIconResource(context, iconView, condition);
     }
 
     @BindingAdapter("visibleGone")
