30536defc2cf4a233e16667083392782ee385962
Tobenna Ezike <Tobeydaniels@gmail.com>
2019-04-01 14:03:50 +0100

    closes#10 and resolves#19
    implement retry and reset rate limiter
    Update readme

--- app/src/main/java/ezike/tobenna/myweather/ui/CurrentWeatherViewModel.java
+++ app/src/main/java/ezike/tobenna/myweather/ui/CurrentWeatherViewModel.java
@@ -3,6 +3,8 @@ package ezike.tobenna.myweather.ui;
 import javax.inject.Inject;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 import ezike.tobenna.myweather.data.local.entity.WeatherResponse;
 import ezike.tobenna.myweather.repository.WeatherRepository;
@@ -10,15 +12,28 @@ import ezike.tobenna.myweather.utils.Resource;
 
 public class CurrentWeatherViewModel extends ViewModel {
 
+    private WeatherRepository mWeatherRepository;
+
+    private MutableLiveData<String> value = new MutableLiveData<>();
+
     private LiveData<Resource<WeatherResponse>> mWeather;
 
     @Inject
     CurrentWeatherViewModel(WeatherRepository repository) {
-        mWeather = repository.loadWeatherResponse();
+        mWeatherRepository = repository;
+        mWeather = Transformations.switchMap(value, input -> mWeatherRepository.loadWeatherResponse(input));
+        setRefreshId(value.getValue());
     }
 
     LiveData<Resource<WeatherResponse>> getCurrentWeather() {
         return mWeather;
     }
 
+    void retry(String input) {
+        setRefreshId(input);
+    }
+
+    private void setRefreshId(String input) {
+        value.setValue(input);
+    }
 }
